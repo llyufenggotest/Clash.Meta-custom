777171b6187fca3c46553653b7e2fcf543be3f40
Chenx Dust <chenx_dust@outlook.com>
2026-06-18 16:18:25 +0800

    chore: make alert from server a warning instead of error

--- transport/anytls/session/session.go
+++ transport/anytls/session/session.go
@@ -307,7 +307,7 @@ func (s *Session) recvLoop() error {
 						return err
 					}
 					if s.isClient {
-						log.Errorln("[Alert from server] %s", string(buffer))
+						log.Warnln("[Alert from server] %s", string(buffer))
 					}
 					pool.Put(buffer)
 					return nil
